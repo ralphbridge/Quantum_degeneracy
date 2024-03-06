9d08135ae1e9a557740664dd365b1aee2255d296
Raúl Puente <rpuente2@huskers.unl.edu>
2024-03-06 17:00:26 -0600

    Update autocorrelator.py
    Changed big profile() function to make execution easier to debug

--- autocorrelator.py
+++ autocorrelator.py
@@ -41,134 +41,136 @@ def S_q(t,alpha,E0):
         j+=1
     return S
 
-def pulse_profile():
-    S=pd.read_excel("spectrum.xlsx").to_numpy()
-    n=np.size(S,0)
+# def pulse_profile():
+    
 
-    lam=np.zeros(np.size(S,0))
-    Il=np.zeros(np.size(S,0))
-    Iluv=np.zeros(np.size(S,0))
-    Ilir=np.zeros(np.size(S,0))
+#     return (lam,spectruml,f,spectrum,Et,t)
 
-    for j in range(0,np.size(S,1)):
-	    for i in range (0,np.size(S,0)):
-		    #print(S[i][j])
-		    if j==0:
-    			lam[i]=(S[i][j])*1e-9
-		    elif j==1:
-    			Il[i]=S[i][j]
-		    elif j==2:
-    			Iluv[i]=S[i][j]
-		    else:
-			    Ilir[i]=S[i][j]
+###############
 
-    Il=(Il-min(Il))/max(Il)
-    Iluv=(Iluv-min(Iluv))/max(Iluv)
-    Ilir=(Ilir-min(Ilir))/max(Ilir)
-    
-    Iltest1=np.exp(-((lam-756e-9)/(2*10e-9))**2) # Single Gaussian
-    Iltest2=np.exp(-((lam-756e-9)/(2*10e-9))**2)+0.65*np.exp(-((lam-815e-9)/(2*15e-9))**2) # Double Gaussian
+S=pd.read_excel("spectrum.xlsx").to_numpy()
+n=np.size(S,0)
 
-    spectruml=Il
-    
-    ######## Interpolating section
-    
-    nint=0 # Number of interpolated iterations: N added points between experimental points is N=2^n-1
+lam=np.zeros(np.size(S,0))
+Il=np.zeros(np.size(S,0))
+Iluv=np.zeros(np.size(S,0))
+Ilir=np.zeros(np.size(S,0))
 
-    for i in range(nint):
-        lamtemp=np.zeros(2*n-1)
-        spectrumltemp=np.zeros(2*n-1)
-    
-        spectruml_interp=interp1d(lam,spectruml)
+for j in range(0,np.size(S,1)):
+    for i in range (0,np.size(S,0)):
+        #print(S[i][j])
+        if j==0:
+            lam[i]=(S[i][j])*1e-9
+        elif j==1:
+            Il[i]=S[i][j]
+        elif j==2:
+            Iluv[i]=S[i][j]
+        else:
+            Ilir[i]=S[i][j]
 
-        for i in range(n):
-            lamtemp[2*i]=lam[i]
-            spectrumltemp[2*i]=spectruml[i]
-            if i<n-1:
-                lamtemp[2*i+1]=(lam[i+1]+lam[i])/2
-                spectrumltemp[2*i+1]=spectruml_interp(lamtemp[2*i+1])
+Il=(Il-min(Il))/max(Il)
+Iluv=(Iluv-min(Iluv))/max(Iluv)
+Ilir=(Ilir-min(Ilir))/max(Ilir)
 
-        n=2*n-1
-        lam=lamtemp
-    
-        del spectruml, spectruml_interp
-    
-        spectruml=spectrumltemp
-    
-        del spectrumltemp
-    
-    #################
+Iltest1=np.exp(-((lam-756e-9)/(2*10e-9))**2) # Single Gaussian
+Iltest2=np.exp(-((lam-756e-9)/(2*10e-9))**2)+0.65*np.exp(-((lam-815e-9)/(2*15e-9))**2) # Double Gaussian
 
-    f=np.zeros(n)
-    spectrum=np.zeros(n)
+spectruml=Iltest2
 
-    for i in range(n):
-        f[n-i-1]=c/(lam[i])
-        spectrum[n-i-1]=(lam[i]**2)*spectruml[i]/c
-    
-    ######## Creating equally spaced frequency domain and spectrum
-    
-    # ftemp=np.linspace(min(f),max(f),n)
-    # spectrumtemp=np.zeros(n)
-    
-    # spectrum_interp=interp1d(f,spectrum)
-    
-    # for i in range(n):
-    #     spectrumtemp[i]=spectrum_interp(ftemp[i])
-    
-    # fig,(ax1,ax2)=plt.subplots(2,1,tight_layout=True)
-    # plt.subplot(2,1,1)
-    # line1,=ax1.plot(f*1e-15,spectrum)
-    # ax1.set_xlabel(r'Frequency $f\ GHz$', fontsize=16)
-    # ax1.set_ylabel('Frequency spectrum', fontsize=16)
-    # ax1.set_xlim([min(f)*1e-15,0.3*max(f)*1e-15])
+######## Interpolating section
 
-    # plt.subplot(2,1,2)
-    # line2,=ax2.plot(ftemp*1e-15,spectrumtemp)
-    # ax2.set_xlabel(r'Interpolated frequency $f_{interp}\ GHz$', fontsize=16)
-    # ax2.set_ylabel('Interpolated frequency spectrum', fontsize=16)
-    # ax2.set_xlim([min(ftemp)*1e-15,0.3*max(ftemp)*1e-15])
-    
-    # f=ftemp
-    # spectrum=spectrumtemp
-    
-    # del ftemp, spectrumtemp
-    
-    # df=f[1]-f[0]
-    
-    ###########
-    
-    df=(max(f)-min(f))/(n-1)
-    
-    t=np.arange(-n/2,n/2)/(n*df)
-    #t=range(n)/(n*df)
+# nint=0 # Number of interpolated iterations: N added points between experimental points is N=2^n-1
 
-    pulse=np.fft.ifftshift(np.fft.ifft(spectrum))
-    
-    It=abs(pulse)
+# for i in range(nint):
+#     lamtemp=np.zeros(2*n-1)
+#     spectrumltemp=np.zeros(2*n-1)
+#     spectruml_interp=interp1d(lam,spectruml)
 
-    fig,(ax1,ax2)=plt.subplots(2,1,tight_layout=True)
-    plt.subplot(2,1,1)
-    line1,=ax1.plot(lam*1e9,spectruml)
-    ax1.set_xlabel(r'Wavelength $\lambda\ nm$', fontsize=16)
-    ax1.set_ylabel('Amplitude', fontsize=16)
-    ax1.set_xlim([min(lam)*1e9,max(lam)*1e9])
+#     for i in range(n):
+#         lamtemp[2*i]=lam[i]
+#         spectrumltemp[2*i]=spectruml[i]
+#         if i<n-1:
+#             lamtemp[2*i+1]=(lam[i+1]+lam[i])/2
+#             spectrumltemp[2*i+1]=spectruml_interp(lamtemp[2*i+1])
 
-    plt.subplot(2,1,2)
-    line2,=ax2.plot(t*1e15,It)
-    ax2.set_xlabel(r'Time $t\ fs$', fontsize=16)
-    ax2.set_ylabel('Amplitude', fontsize=16)
-    ax2.set_xlim([-100,100])
+#     n=2*n-1
+#     lam=lamtemp
+#     del spectruml, spectruml_interp
+#     spectruml=spectrumltemp
+#     del spectrumltemp
 
-    plt.show()
-    fig.savefig("10fs.pdf",bbox_inches='tight')
+#################
 
-    Et=np.sqrt(2*It/(c*eps0))
-    Et=E00*Et/max(Et)
+f=np.zeros(n)
+spectrum=np.zeros(n)
 
-    return (lam,spectruml,f,spectrum,Et,t)
+for i in range(n):
+    f[n-i-1]=c/(lam[i])
+    spectrum[n-i-1]=(lam[i]**2)*spectruml[i]/c
 
-lam,spectruml,f,spectrum,E0,t=pulse_profile()
+######## Creating equally spaced frequency domain and spectrum
+
+# ftemp=np.linspace(min(f),max(f),n)
+# spectrumtemp=np.zeros(n)
+
+# spectrum_interp=interp1d(f,spectrum)
+
+# for i in range(n):
+#     spectrumtemp[i]=spectrum_interp(ftemp[i])
+
+# fig,(ax1,ax2)=plt.subplots(2,1,tight_layout=True)
+# plt.subplot(2,1,1)
+# line1,=ax1.plot(f*1e-15,spectrum)
+# ax1.set_xlabel(r'Frequency $f\ GHz$', fontsize=16)
+# ax1.set_ylabel('Frequency spectrum', fontsize=16)
+# ax1.set_xlim([min(f)*1e-15,0.3*max(f)*1e-15])
+
+# plt.subplot(2,1,2)
+# line2,=ax2.plot(ftemp*1e-15,spectrumtemp)
+# ax2.set_xlabel(r'Interpolated frequency $f_{interp}\ GHz$', fontsize=16)
+# ax2.set_ylabel('Interpolated frequency spectrum', fontsize=16)
+# ax2.set_xlim([min(ftemp)*1e-15,0.3*max(ftemp)*1e-15])
+
+# f=ftemp
+# spectrum=spectrumtemp
+
+# del ftemp, spectrumtemp
+
+# df=f[1]-f[0]
+
+###########
+
+df=(max(f)-min(f))/(n-1)
+
+t=np.arange(-n/2,n/2)/(n*df)
+#t=range(n)/(n*df)
+
+pulse=np.fft.ifftshift(np.fft.ifft(spectrum))
+
+It=abs(pulse)
+
+fig,(ax1,ax2)=plt.subplots(2,1,tight_layout=True)
+plt.subplot(2,1,1)
+line1,=ax1.plot(lam*1e9,spectruml)
+ax1.set_xlabel(r'Wavelength $\lambda\ nm$', fontsize=16)
+ax1.set_ylabel('Amplitude', fontsize=16)
+ax1.set_xlim([min(lam)*1e9,max(lam)*1e9])
+
+plt.subplot(2,1,2)
+line2,=ax2.plot(t*1e15,It)
+ax2.set_xlabel(r'Time $t\ fs$', fontsize=16)
+ax2.set_ylabel('Amplitude', fontsize=16)
+ax2.set_xlim([-100,100])
+
+plt.show()
+fig.savefig("10fs.pdf",bbox_inches='tight')
+
+E0=np.sqrt(2*It/(c*eps0))
+E0=E00*E0/max(E0)
+
+###############
+
+#lam,spectruml,f,spectrum,E0,t=pulse_profile()
 
 Efield=np.multiply(E0,np.cos((w+alpha*t)*t))
 #n=10000
