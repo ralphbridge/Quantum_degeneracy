519915e8c6749a701729e65ef7be5ed7c008b0bb
Raúl Puente <rpuente2@huskers.unl.edu>
2024-06-08 21:12:15 -0500

    Update GaussianFit.py
    Tidy it up a bit

--- GaussianFit.py
+++ GaussianFit.py
@@ -1,22 +1,17 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as scipy
-from scipy import optimize
-from matplotlib.ticker import AutoMinorLocator
-from matplotlib import gridspec
-import matplotlib.ticker as ticker
 import pandas as pd
 
 def _1gaussian(x, amp1,cen1,sigma1):
     return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x_array-cen1)/sigma1)**2)))
 
 def _3gaussian(x, amp1,cen1,sigma1, amp2,cen2,sigma2, amp3,cen3,sigma3, amp4,cen4,sigma4, amp5,cen5,sigma5):
-    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x_array-cen1)/sigma1)**2))) + \
-            amp2*(1/(sigma2*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x_array-cen2)/sigma2)**2)))+ \
-            amp3*(1/(sigma3*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x_array-cen3)/sigma3)**2)))+ \
-            amp4*(1/(sigma4*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x_array-cen4)/sigma4)**2)))+ \
-            amp5*(1/(sigma5*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x_array-cen5)/sigma5)**2)))
+    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen1)/sigma1)**2))) + \
+            amp2*(1/(sigma2*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen2)/sigma2)**2)))+ \
+            amp3*(1/(sigma3*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen3)/sigma3)**2)))+ \
+            amp4*(1/(sigma4*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen4)/sigma4)**2)))+ \
+            amp5*(1/(sigma5*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen5)/sigma5)**2)))
             
 S=pd.read_excel("pulse.xlsx").to_numpy()
 
@@ -72,7 +67,7 @@ ax1.set_ylabel("y_array",family="serif",  fontsize=12)
 
 plt.subplot(2,1,2)
 
-ax2.plot(x_array*1e15,_3gaussian(x_array, *popt_2gauss),'.k')
+ax2.plot(x_array*1e15,myfit,'.k')
 ax2.plot(x_array*1e15,y_array_2gauss)
 
 ax2.set_xlabel("x_array",family="serif",  fontsize=12)
