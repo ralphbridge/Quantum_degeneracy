51f9882317faa2b0a7d71a61ab9134711e8c8424
Raúl Puente <rpuente2@huskers.unl.edu>
2024-06-08 21:11:35 -0500

    Update autocorrelator.py
    Added Gaussian fit and started working on the chirp part for the widening

--- autocorrelator.py
+++ autocorrelator.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy as scipy
-from scipy.interpolate import interp1d
-from scipy import optimize
 
 eps0=8.85e-12
 c=3e8
@@ -29,13 +27,7 @@ def E0shift(E0,j):
     del Eftemp
     return Ef
 
-# def S_l(t,alpha,E0):
-#     S=np.zeros(len(t))
-#     S=2*np.trapz((E(t,alpha,E0))**2,t)
-#     S+=2*np.convolve(E(t,alpha,E0),E(t,alpha,E0),mode='same')
-#     return S
-
-def S_q(t,alpha,E0):
+def S_q(t,E0):
     S=np.zeros(len(t))
     j=0
     for tau in t:            
@@ -43,10 +35,15 @@ def S_q(t,alpha,E0):
         j+=1
     return S
 
-def _3gaussian(t, amp1,cen1,sigma1, amp2,cen2,sigma2, amp3,cen3,sigma3):
-    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((t-cen1)/sigma1)**2))) + \
-            amp2*(1/(sigma2*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((t-cen2)/sigma2)**2)))+ \
-            amp3*(1/(sigma3*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((t-cen3)/sigma3)**2)))
+def _1gaussian(x, amp1,cen1,sigma1):
+    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen1)/sigma1)**2)))
+
+def _3gaussian(x, amp1,cen1,sigma1, amp2,cen2,sigma2, amp3,cen3,sigma3, amp4,cen4,sigma4, amp5,cen5,sigma5):
+    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen1)/sigma1)**2))) + \
+            amp2*(1/(sigma2*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen2)/sigma2)**2)))+ \
+            amp3*(1/(sigma3*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen3)/sigma3)**2)))+ \
+            amp4*(1/(sigma4*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen4)/sigma4)**2)))+ \
+            amp5*(1/(sigma5*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen5)/sigma5)**2)))
 
 ###############
 
@@ -154,45 +151,62 @@ E0=E00*E0/max(E0)
 
 ############### Gaussian fit (3 Gaussian functions)
 
-amp1=max(It)
-sigma1=10e-15
-cen1=t[np.argmin(It)]
+sigma1=1e-15
+amp1=max(It)*sigma1
+cen1=t[np.argmax(It)]
 
-amp2=max(It)/5
-sigma2=20e-15
-cen2=t[np.argmin(It)]//4
+sigma2=5e-15
+amp2=(max(It)/5)*sigma2
+cen2=-2e-14
 
-amp3=max(It)/5
-sigma3=20e-15
-cen3=3*t[np.argmin(It)]//4
+sigma3=5e-15
+amp3=(max(It)/5)*sigma3
+cen3=2e-14
 
-ttest=0
-Ittest=0
+sigma4=5e-15
+amp4=(max(It)/5)*sigma4
+cen4=-1e-14
 
-for i in range(len(t)):
-    if t[i]>-50e-15 and t[i]<50e-15:
-        ttest=np.append(ttest,t[i])
-        Ittest=np.append(Ittest,It[i])
+sigma5=5e-15
+amp5=(max(It)/5)*sigma5
+cen5=1e-14
 
-ttest=np.delete(ttest,0)        
-Ittest=np.delete(Ittest,0)
+popt_2gauss, pcov_2gauss = scipy.optimize.curve_fit(_3gaussian, t, It, p0=[amp1, cen1, sigma1, amp2, cen2, sigma2, amp3, cen3, sigma3, amp4, cen4, sigma4, amp5, cen5, sigma5])
 
-popt_2gauss, pcov_2gauss = scipy.optimize.curve_fit(_3gaussian, ttest, Ittest, p0=[amp1, cen1, sigma1, amp2, cen2, sigma2, amp3, cen3, sigma3])
+perr_2gauss = np.sqrt(np.diag(pcov_2gauss))
 
-Itfit=_3gaussian(ttest, *popt_2gauss)
+pars_1 = popt_2gauss[0:3]
+pars_2 = popt_2gauss[3:6]
+pars_3 = popt_2gauss[6:9]
+pars_4 = popt_2gauss[9:12]
 
-fig,(ax1,ax2)=plt.subplots(2,1,tight_layout=True)
-plt.subplot(2,1,1)
-line1,=ax1.plot(ttest*1e15,Ittest)
-ax1.set_xlabel(r'Time $t\ fs$', fontsize=16)
-ax1.set_ylabel('Amplitude', fontsize=16)
-ax1.set_xlim([-50,50])
+pars_1=pars_4
 
-plt.subplot(2,1,2)
-line2,=ax2.plot(ttest*1e15,Itfit)
-ax2.set_xlabel(r'Time $t\ fs$', fontsize=16)
-ax2.set_ylabel('Amplitude', fontsize=16)
-ax2.set_xlim([-50,50])
+del pars_4
+
+amp1=pars_1[0]
+cen1=pars_1[1]
+sigma1=pars_1[2]
+
+amp2=pars_2[0]
+cen2=pars_2[1]
+sigma2=pars_2[2]
+
+amp3=pars_3[0]
+cen3=pars_3[1]
+sigma3=pars_3[2]
+
+del pars_1,pars_2,pars_3
+
+It_fit=2*_1gaussian(t,amp1,cen1,sigma1)+_1gaussian(t,amp2,cen2,sigma2)+_1gaussian(t,amp3,cen3,sigma3)
+
+plt.plot(t*1e15,It_fit,'.k')
+plt.plot(t*1e15,It)
+
+plt.xlabel("t fs",family="serif",  fontsize=12)
+plt.ylabel("Intensity",family="serif",  fontsize=12)
+
+plt.xlim([-100,100])
 
 plt.show()
 
@@ -204,10 +218,8 @@ tt=np.linspace(min(t),max(t),n)
 t=tt
 T1=np.sqrt(1+(gdd/T0**2)**2)*T0
 Efield=(E00/(np.sqrt(2*np.pi)*T1))*np.exp(-t**2/(2*T1**2))*np.cos(w*t+alpha*t**2)
-#Slin=S_l(t,alpha,E0)
-Squad=S_q(t,alpha,Efield)
+Squad=S_q(t,Efield)
 
-# Create the figure and the line that we will manipulate
 fig,(ax1,ax3)=plt.subplots(2,1,tight_layout=True)
 
 line1,=ax1.plot(t*1e15,Efield,lw=1)
