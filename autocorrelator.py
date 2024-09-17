5c0d6b3c5180f1fe3891616f7b5f2f3e8ac1e979
ralphbridge <ralphqm7@gmail.com>
2024-09-17 16:17:09 -0500

    Inverse Fourier transform does not work

--- autocorrelator.py
+++ autocorrelator.py
@@ -85,15 +85,15 @@ for j in range(0,np.size(S,1)):
 Il=(Il-min(Il))/max(Il) # Initial measured spectrum
 
 f=np.zeros(n)
-spectrum=np.zeros(n)
+spectrumf=np.zeros(n)
 
 for i in range(n):
     f[n-i-1]=c/(lam[i])
-    spectrum[n-i-1]=(lam[i]**2)*Il[i]/c
-    
+    spectrumf[n-i-1]=(lam[i]**2)*Il[i]/c
+
 fs=max(f)-min(f) # Sampling frequency (equals the bandwidth or maximum frequency)
 t0=np.arange(-n/2,n/2)/fs
-Et0=np.fft.ifftshift(np.fft.ifft(np.sqrt(2*spectrum/(c*eps0)))) # Initial electric field spectrum
+Et0=np.fft.ifftshift(np.fft.ifft(np.sqrt(2*spectrumf/(c*eps0)))) # Initial electric field spectrum
 
 ########################## Trimming spectrum low and high frequencies and interpolating it to get equally spaced frequencies
 
@@ -103,7 +103,7 @@ spectrumw=np.zeros(NN)
 
 for i in range(0,NN):
     w[i]=2*np.pi*f[i+266] # 266 is the position of the original spectrum where it starts being bigger than 0
-    spectrumw[i]=spectrum[i+266]
+    spectrumw[i]=2*np.pi*spectrumf[i+266] # New angular frequency spectrum (trimmed)
     
 spectrumw_interp_function=inter.CubicSpline(w,spectrumw)
 
@@ -126,7 +126,8 @@ for i in range(N):
         if spectrumw_interp[i]<0:
             spectrumw_interp[i]=0
 
-Ef0=np.sqrt(2*spectrumw_interp/(c*eps0))*(1+0j) # Initial electric field spectrum with trimmed spectrum
+spectrum_interp=spectrumw_interp/(2*np.pi)
+Ef0=np.sqrt(2*spectrum_interp/(c*eps0))*(1+0j) # Initial electric field spectrum (trimmed)
 
 # Initial phases for original measured spectrum (up to third order)
 # i=0
@@ -134,6 +135,23 @@ Ef0=np.sqrt(2*spectrumw_interp/(c*eps0))*(1+0j) # Initial electric field spectru
 #     Ef0[i]=Ef0[i]*np.exp(1j*GDD_laser[i]*(w-w0)**2/math.factorial(2))
 #     Ef0[i]=Ef0[i]*np.exp(1j*TOD_laser[i]*(w-w0)**3/math.factorial(3))
 #     i+=1
+
+######################## Increasing time resolution (by increasing frequency range)
+
+f_interp=f
+
+# NN=1000
+# f_interp=np.append(f_interp,np.linspace(max(f)+df,max(f)+NN*df,NN))
+# Et0_interp=np.append(Ef0,np.zeros(NN))
+
+NN=len(f_interp)
+
+######################################
+
+fs=max(f_interp)-min(f_interp) # Sampling frequency (equals the bandwidth or maximum frequency)
+
+t0_interp=np.arange(-NN/2,NN/2)/fs
+Et0_interp=np.fft.ifftshift(np.fft.ifft(Ef0)) # Initial electric field
 
 ########################## Getting GDD data from Thorlabs chirped mirrors data
 
@@ -149,9 +167,9 @@ for j in range(np.size(CM,1)):
         else:
             GD_cm_lam[i]=(CM[i][j])*1e-15
 
-plt.plot(lam_cm,GD_cm_lam)
-plt.xlabel('wavelength')
-plt.show()
+# plt.plot(lam_cm,GD_cm_lam)
+# plt.xlabel('wavelength')
+# plt.show()
 
 w_cm=np.zeros(n_cm)
 GD_cm=np.zeros(n_cm)
@@ -163,14 +181,14 @@ GD_cm_interp_function=inter.CubicSpline(w_cm,GD_cm)
 GD_cm_interp=GD_cm_interp_function(w)
 # GD_cm_interp=np.interp(w,w_cm,GD_cm)
 
-plt.plot(w_cm,GD_cm*1e15,'o')
-plt.plot(w,GD_cm_interp*1e15)
-plt.xlim([2e15,3e15])
-plt.ylim([0,75])
-plt.xlabel(r'Angular frequency $\omega\ rad/s$', fontsize=16)
-plt.ylabel(r'GD $fs$', fontsize=16)
-plt.grid()
-plt.show()
+# plt.plot(w_cm,GD_cm*1e15,'o')
+# plt.plot(w,GD_cm_interp*1e15)
+# plt.xlim([2e15,3e15])
+# plt.ylim([0,75])
+# plt.xlabel(r'Angular frequency $\omega\ rad/s$', fontsize=16)
+# plt.ylabel(r'GD $fs$', fontsize=16)
+# plt.grid()
+# plt.show()
 
 GDD_cm=np.zeros(N)
 TOD_cm=np.zeros(N)
@@ -188,12 +206,12 @@ for i in range(N): # Higher accuracy order derivatives from Fornberg 1988
         GDD_cm[i]=(GD_cm_interp[i-2]-8*GD_cm_interp[i-1]+8*GD_cm_interp[i+1]-GD_cm_interp[i+2])/(12*2*np.pi*df)
         TOD_cm[i]=(-GD_cm_interp[i-2]+16*GD_cm_interp[i-1]-30*GD_cm_interp[i]+16*GD_cm_interp[i+1]-GD_cm_interp[i+2])/(12*(2*np.pi*df)**2)
 
-plt.plot(w,GDD_cm*1e30)
-plt.ylim([-200,200])
-plt.xlabel(r'Angular frequency $\omega\ rad/s$', fontsize=16)
-plt.ylabel(r'Interpolated GDD $fs^2$', fontsize=16)
-plt.grid()
-plt.show()
+# plt.plot(w,GDD_cm*1e30)
+# plt.ylim([-200,200])
+# plt.xlabel(r'Angular frequency $\omega\ rad/s$', fontsize=16)
+# plt.ylabel(r'Interpolated GDD $fs^2$', fontsize=16)
+# plt.grid()
+# plt.show()
 
 ########################## Getting GDD data from Thorlabs P01 mirrors data
 
@@ -215,7 +233,8 @@ for i in range(n_p01):
     w_p01[n_p01-1-i]=2*np.pi*c/lam_p01[i]
     GDD_p01_data[n_p01-1-i]=GDD_p01_data_lam[i]
 
-GDD_p01_interp=inter.CubicSpline(w_p01,GDD_p01_data)
+GDD_p01_interp_function=inter.CubicSpline(w_p01,GDD_p01_data)
+GDD_p01_interp=GDD_p01_interp_function(w)
 
 TOD_p01_data=np.zeros(n_p01)
 for i in range(0,n_p01):
@@ -230,15 +249,10 @@ for i in range(0,n_p01):
         m2=(GDD_p01_data[i+1]-GDD_p01_data[i])/(w_p01[i+1]-w_p01[i])
         TOD_p01_data[i]=(m1+m2)/2
 
-TOD_p01_interp=inter.CubicSpline(w_p01,GDD_p01_data)
-TOD_p01=np.zeros(N)
+TOD_p01_interp_function=inter.CubicSpline(w_p01,GDD_p01_data)
+TOD_p01_interp=TOD_p01_interp_function(w)
 
 #print(GDD_p01_interp(2*np.pi*c/800e-9)*1e30) # Check this line, it does not interpolate correctly
-GDD_p01=np.zeros(N)
-TOD_p01=np.zeros(N)
-for i in range(len(f)):
-    GDD_p01[i]=GDD_p01_interp(w[i])
-    TOD_p01[i]=TOD_p01_interp(w[i])
 
 # plt.plot(2*np.pi*f,GDD_p01*1e30)
 # plt.xlim([2e15,3e15])
@@ -250,44 +264,60 @@ for i in range(len(f)):
 
 ############################## Computing the phases due to each element in the layout
 
-Ef=np.zeros(np.size(Ef0),dtype=np.complex_)
+Ef=np.zeros(np.size(Ef0),dtype=np.complex128)
 
 for i in range(N):
     # Dispersion phases introduced by air up to the third order
     Ef[i]=Ef0[i]*np.exp(1j*kp0_air*zd)
-    # Ef[i]=Ef[i]np.exp(1j*kp0_air*(w[i]-w0)*zd)
-    # Ef[i]=Ef[i]*np.exp(1j*kpp0_air*(w[i]-w0)**2*zd/math.factorial(2))
-    # Ef[i]=Ef[i]*np.exp(1j*kppp0_air*(w[i]-w0)**3*zd/math.factorial(3))
+    Ef[i]=Ef[i]*np.exp(1j*kp0_air*(w[i]-w0)*zd)
+    Ef[i]=Ef[i]*np.exp(1j*kpp0_air*(w[i]-w0)**2*zd/math.factorial(2))
+    Ef[i]=Ef[i]*np.exp(1j*kppp0_air*(w[i]-w0)**3*zd/math.factorial(3))
     
     # Dispersion phases introduced by BK7 Fused Silica glass window up to the third order
     Ef[i]=Ef[i]*np.exp(1j*kp0_bk7*zd)
-    # Ef[i]=Ef[i]*np.exp(1j*kp0_bk7*(w[i]-w0)*zw)
-    # Ef[i]=Ef[i]*np.exp(1j*kpp0_bk7*(w[i]-w0)**2*zw/math.factorial(2))
-    # Ef[i]=Ef[i]*np.exp(1j*kppp0_bk7*(w[i]-w0)**3*zw/math.factorial(3))
+    Ef[i]=Ef[i]*np.exp(1j*kp0_bk7*(w[i]-w0)*zw)
+    Ef[i]=Ef[i]*np.exp(1j*kpp0_bk7*(w[i]-w0)**2*zw/math.factorial(2))
+    Ef[i]=Ef[i]*np.exp(1j*kppp0_bk7*(w[i]-w0)**3*zw/math.factorial(3))
     
     # # Dispersion phases introduced by bounces off Chirped mirrors up to the third order
     # Ef[i]=Ef[i]*np.exp(1j*GDD_cm[i]*(w[i]-w0)**2/math.factorial(2))
     # Ef[i]=Ef[i]*np.exp(1j*TOD_cm[i]*(w[i]-w0)**3/math.factorial(3))
+    Ef[i]=Ef[i]*np.exp(1j*GD_cm_interp[i]*(w[i]-w0))
     
     # # Dispersion phases introduced by bounces off P01 Silver mirrors up to the third order
     # Ef[i]=Ef[i]*np.exp(1j*GDD_p01[i]*(w[i]-w0)**2/math.factorial(2))
     # Ef[i]=Ef[i]*np.exp(1j*TOD_p01[i]*(w[i]-w0)**3/math.factorial(3))
+    Ef[i]=Ef[i]*np.exp(1j*0.5*GDD_p01_interp[i]*(w[i]-w0)**2)
 
-######################## Increasing time resolution (by increasing frequency range) by 3^N <--------- DOES NOT WORK
-
-# NN=0
-# df=f[1]-f[0]
-
-# for i in range(NN):
-#     f=np.append(np.append(np.arange(min(f)-n*df,min(f),df),f),np.arange(max(f)+df,max(f)+(n+1)*df,df))
-#     Ef=np.append(np.append(np.zeros(n),Ef),np.zeros(n))
-#     N=len(f)
+######################## Increasing time resolution (by increasing frequency range)
 
 NN=1000
 f=np.append(f,np.linspace(max(f)+df,max(f)+NN*df,NN))
 Ef=np.append(Ef,np.zeros(NN))
 
 N=len(f)
+
+fig,(ax1,ax2)=plt.subplots(2,1,tight_layout=True)
+plt.subplot(2,1,1)
+line1,=plt.plot(t0*1e15,Et0)
+ax1.set_xlim([-20,20])
+ax1.set_xlabel(r'Time $t\ fs$', fontsize=16)
+ax1.set_ylabel(r'$E_0\ V/m$', fontsize=16)
+# major_tick = np.arange(roundup(min(lam)*1e9), roundup(max(lam)*1e9),200)#[200, 400, 600, 800, 1000]
+# minor_tick = np.arange(roundup(min(lam)*1e9)+100, roundup(max(lam)*1e9),200)#[300, 500, 700, 900]
+# ax1.set_xticks(major_tick) # Grid
+# ax1.set_xticks(minor_tick, minor=True)
+ax1.grid(which='both')
+
+plt.subplot(2,1,2)
+line2,=ax2.plot((t0_interp)*1e15,Et0_interp)
+ax2.set_xlabel(r'Time $t\ fs$', fontsize=16)
+ax2.set_ylabel('$E_0^{interp}\ V/m$', fontsize=16)
+ax2.set_xlim([-20,20])
+ax2.grid(which='both')
+
+# fig.savefig("Frequency_Time.pdf",bbox_inches='tight')
+plt.show()
 
 ##################################################
 
@@ -302,7 +332,6 @@ line1,=plt.plot(t0*1e15,Et0)
 ax1.set_xlim([-20,20])
 ax1.set_xlabel(r'Time $t\ fs$', fontsize=16)
 ax1.set_ylabel(r'$E_0\ V/m$', fontsize=16)
-
 # major_tick = np.arange(roundup(min(lam)*1e9), roundup(max(lam)*1e9),200)#[200, 400, 600, 800, 1000]
 # minor_tick = np.arange(roundup(min(lam)*1e9)+100, roundup(max(lam)*1e9),200)#[300, 500, 700, 900]
 # ax1.set_xticks(major_tick) # Grid
@@ -310,7 +339,7 @@ ax1.set_ylabel(r'$E_0\ V/m$', fontsize=16)
 ax1.grid(which='both')
 
 plt.subplot(2,1,2)
-line2,=ax2.plot(t*1e15,Et)
+line2,=ax2.plot((t-t[np.argmax(Et)])*1e15,Et)
 ax2.set_xlabel(r'Time $t\ fs$', fontsize=16)
 ax2.set_ylabel('$E\ V/m$', fontsize=16)
 ax2.set_xlim([-100,100])
@@ -329,14 +358,14 @@ plt.show()
 Slinear=S_l(t,Et)
 Squad=S_q(t,Et)
 
-fig,(ax1,ax3)=plt.subplots(2,1,tight_layout=True)
+fig,(ax3)=plt.subplots(1,1,tight_layout=True)
 
-line1,=ax1.plot(t*1e15,Et,lw=1)
-ax1.plot(t*1e15,Et,'.',alpha=0.01)
-ax1.set_xlabel(r'Time $t\ fs$', fontsize=16)
-ax1.set_ylabel(r'$E\ V/m$', fontsize=16)
-ax1.set_xlim([-50,50])
-ax1.grid()
+# line1,=ax1.plot(t*1e15,Et,lw=1)
+# ax1.plot(t*1e15,Et,'.',alpha=0.01)
+# ax1.set_xlabel(r'Time $t\ fs$', fontsize=16)
+# ax1.set_ylabel(r'$E\ V/m$', fontsize=16)
+# ax1.set_xlim([-50,50])
+# ax1.grid()
 
 # line2,=ax2.plot(t*1e15 ,Slinear,lw=1)
 # ax2.set_xlabel(r'Time delay $\tau\ fs$', fontsize=16)
@@ -362,5 +391,3 @@ plt.show()
 # Check difference between calculated and experimental total GDD
 # Discuss Agrawal's expressions with Herman
 # Get theoretical expression for Fourier transform (for simple Gaussian spectrum)
-
-# Test for github access
